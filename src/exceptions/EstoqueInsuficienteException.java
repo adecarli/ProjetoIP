d9989ff9f3faaf1d58f9a8df6dc17ad9dde8fb47
André De' Carli da Mota Silveira <acms@cin.ufpe.br>
2016-06-21 17:06:25 -0300

    Add files via upload

--- src/exceptions/EstoqueInsuficienteException.java
+++ src/exceptions/EstoqueInsuficienteException.java
@@ -1,9 +1,9 @@
-package Fachada;
+package exceptions;
 
 public class EstoqueInsuficienteException extends Exception {
 	private int quantidade;
 	public EstoqueInsuficienteException(int quantidade){
+		super("Estoque Insuficiente. Estoque atual: "+ quantidade);
 		this.quantidade=quantidade;
-		super("Estoque Insuficiente. Estoque atual: "+this.quantidade);
 	}
 }
