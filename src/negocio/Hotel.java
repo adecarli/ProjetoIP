74475f633231283eabdd5d7b25870f8e4135b562
menezesmalu <mlmv@cin.ufpe.br>
2016-06-21 13:44:03 -0300

    Malu

--- src/negocio/Hotel.java
+++ src/negocio/Hotel.java
@@ -40,7 +40,7 @@ public class Hotel {
 	}
 	public void checkin(String numeroQuarto, String cpfCliente, int numDias) throws QuartoNaoEncontradoException, ClienteNaoEncontradoException, QuartoOcupadoException {
 		QuartoAbstrato quarto = cadQuartos.procurar(numeroQuarto);
-		Cliente cliente = cadClientes.procurar(cpfCliente);
+		Cliente cliente = cadClientes.procurarCliente(cpfCliente);
 		quarto.checkin(cliente, numDias);
 	}
 	//Este metodo relaciona QuartoAbstrato e Cliente
@@ -49,12 +49,13 @@ public class Hotel {
 	public double checkout(String numeroQuarto) throws QuartoNaoEncontradoException, QuartoVazioException {
 		QuartoAbstrato quarto = cadQuartos.procurar(numeroQuarto);
 		double total = quarto.checkout();
+		
 		return total;
 	}
 	//Este metodo relaciona QuartoAbstrato e Funcionario
 	public void limparQuarto(String numeroQuarto, String cpfFuncionario, double gorjeta) throws QuartoNaoEncontradoException, FuncionarioNaoEncontradoException {
 		QuartoAbstrato quarto = cadQuartos.procurar(numeroQuarto);
-		Funcionario funcionario = cadFuncionarios.procurar(cpfFuncionario);
+		Funcionario funcionario = cadFuncionarios.procurarFuncionario(cpfFuncionario);
 		funcionario.trabalhar(gorjeta);
 		quarto.limpar();
 	}
@@ -62,24 +63,70 @@ public class Hotel {
 	//Metodos relacionados a Funcionario
 	
 	public void cadastrarFuncionario(Funcionario funcionario) throws FuncionarioJaCadastradoException {
-		cadFuncionarios.cadastrar(funcionario);
+		cadFuncionarios.cadastrarFuncionario(funcionario);
 	}
 	
 	public void atualizarFuncionario(Funcionario funcionario) throws FuncionarioNaoEncontradoException{
-		cadFuncionarios.atualizar(funcionario);
+		cadFuncionarios.atualizarFuncionario(funcionario);
 	}
 	
 	public void removerFuncionario(String CPF) throws FuncionarioNaoEncontradoException{
-		cadFuncionarios.remover(CPF);
+		cadFuncionarios.removerFuncionario(CPF);
 	}
 	
 	public Funcionario procurarFuncionario(String CPF) throws FuncionarioNaoEncontradoException{
-		return cadFuncionarios.procurar(CPF);
+		return cadFuncionarios.procurarFuncionario(CPF);
 	} 
 	
 	//Metodos relacionados a Cliente
 	
-	public void cadastrarCliente(Cliente cliente) throws ClienteJaCadastradoException{
-		cadClientes.cadastrar(cliente);
+	public void cadastrarCliente(String nome, String cpf) throws ClienteJaCadastradoException {
+		Cliente cliente = new Cliente(nome, cpf);
+		cadClientes.cadastrarCliente(cliente);
 	}
+	
+	public void atualizarCliente(String nome, String cpf) throws ClienteNaoEncontradoException {
+		Cliente cliente = new Cliente(nome, cpf);
+		cadClientes.atualizarCliente(cliente);
+	}
+	public void removerCliente(String cpf) throws ClienteNaoEncontradoException {
+		cadClientes.removerCliente(cpf);
+	}
+	
+	/*public void checkoutCliente(String cpf) throws ClienteNaoEncontradoException {
+		double gastos = cadQuartos.getQuarto(cpf)
+		cadClientes.adicionarGastosCliente(CPF, gastos);
+	}*/
+	
+	public void fazerPedido(String cpf, String produto, int qtde) throws ProdutoNaoCadastradoException, ClienteNaoEncontradoException, PrecoInvalidoException{
+		Produto pedido = cadProdutos.procurarProduto(produto);
+		double valor = pedido.getPreco()*qtde;
+		cadClientes.adicionarGastosCliente(cpf, valor);
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
