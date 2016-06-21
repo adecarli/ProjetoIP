67b00badb2f6e45445693aee5d8c9472c398f3d6
castrPS <rps4@cin.ufpe.br>
2016-06-21 12:42:44 -0300

    CadastroProdutos
    Falta pedido

--- src/negocio/CadastroProdutos.java
+++ src/negocio/CadastroProdutos.java
@@ -1,44 +1,43 @@
 package negocio;
-import classesbasicas.*;
-import exceptions.*;
+import classesBasicas.*;
 import dados.*;
-public class CadastroProdutos {
-	RepositorioProdutos produtos;
-public void cadastrarProduto(Produto produto){
-	try{
+import exceptions.*;
+public class CadastroProdutos{
+	RepositorioProdutos produtos= new RepositorioProdutosArray();
+	RepositorioClientes clientes= new RepositorioClientesArray();
+public void cadastrarProduto (String nome, double preco, int quantidade) throws ProdutoJaCadastradoException, ProdutoNaoCadastradoException{
+	Produto produto= new Produto (nome, preco, quantidade);
+	Produto compara=produtos.procurar(nome);
+	if(compara!=null){
+		throw new ProdutoJaCadastradoException();
+	}
+	else{
 		produtos.cadastrar(produto);
-	}catch(ProdutoJaCadastradoException e){
-		System.out.println(e.getMessage());
 	}
 }
-public Produto procurarProduto(String nome){
-	Produto produto=null;
-	try{
-		produto=produtos.procurar(nome);
-	}catch(ProdutoNaoCadastradoException e){
-		System.out.println(e.getMessage());
-	}
-	return produto;
-}
-public void removerProduto(String nome){
-	try{
-		produtos.remover(nome);
-	}catch(ProdutoNaoCadastradoException e){
-		System.out.println(e.getMessage());
+public void removerProduto(String nome) throws ProdutoNaoCadastradoException{
+	Produto produto=produtos.procurar(nome);
+	boolean removido=produtos.remover(produto);
+	if(!removido){
+		throw new ProdutoNaoCadastradoException();
 	}
 }
-public void renovarEstoque(String nome, int quantidade){
-	try{
-		produtos.renovarEstoque(nome, quantidade);
-	}catch(ProdutoNaoCadastradoException e){
-		System.out.println(e.getMessage());
+public void atualizarPreco(String nome, double preco) throws ProdutoNaoCadastradoException{
+	Produto produto=produtos.procurar(nome);
+	if (produto.getNome().equals(nome)){
+		produtos.atualizarPreco(produto, preco);
+	}
+	else{
+		throw new ProdutoNaoCadastradoException();
 	}
 }
-public void atualizarPreco(String nome, double preco){
-	try{
-		produtos.atualizarPreco(nome,preco);
-	}catch(ProdutoNaoCadastradoException e){
-		System.out.println(e.getMessage());
+public void renovarEstoque(String nome, int quantidade) throws ProdutoNaoCadastradoException{
+	Produto produto=produtos.procurar(nome);
+	if (produto.getNome().equals(nome)){
+		produtos.renovarEstoque(produto, quantidade);
+	}
+	else{
+		throw new ProdutoNaoCadastradoException();
 	}
 }
 }
